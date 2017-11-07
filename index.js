beb2a571ccb51b5f51ab669e09575c628133630f
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-11-07 23:23:09 +0100

    my googleStrategy commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy())
+
+//clientId: 150217203825-iihcm0v50kn81rjhe6lqqpptprkfa8u1.apps.googleusercontent.com
+//clientSecret:-LOsWt0gWjn_u8XxaimBj-3i
 
 app.get('/', (req, res) => {
   res.send ({hi: 'there'})
