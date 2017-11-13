1fe738df7e5b857ca8d78ef0dbc2dfde793bd37c
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-11-13 19:19:12 +0100

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+  googleClientID: '150217203825-iihcm0v50kn81rjhe6lqqpptprkfa8u1.apps.googleusercontent.com',
+  googleClientSecret: '0gWjn_u8XxaimBj-3i-LOsWt',
+  mongoURI: 'mongodb://chibaba-dev:password@ds251245.mlab.com:51245/chibaba-dev',
+  cookieKey: 'djjncsajnhbchuabhfcsahbchsachsgdgdg'
+  };
