233feb0a23690c9ddc1848660fc6b700bfa164ad
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-11-13 20:24:44 +0100

    tweak google setting

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
   {
   clientID: Keys.googleClientID,
   clientSecret: Keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/auth/google/callback',
+  proxy: true
 },
   (accessToken, refreshToken, profile, done ) => {
     User.findOne({googleId: profile.id}).then((existingUser) =>{
