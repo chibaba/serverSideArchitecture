4ac2beefce18fbee73dcfd8009a1d2c5bdf00ab3
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-12-02 22:02:06 +0100

    modified the client app

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json())
    //if it dosent recognise the route
    const path = require('path');
    app.get('*', (req, res) => {
-     res.sendFile(path.resolve(_dirnanme, 'client', 'build', 'index.html'));
+     res.sendFile(path.resolve(__dirnanme, 'client', 'build', 'index.html'));
    })
  }
 
