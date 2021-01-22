91d2c5cfaccebe94d2f969883c1569fcf04c077e
techtheriac <mavyjayann@gmail.com>
2021-01-22 10:54:05 +0100

    modify changes

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -7,7 +7,7 @@ const requireLogin = require('../middleware/requireLogin')
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res)=> {
        
-    const charge = await stripe.charges.create({
+    const charge = await stripe.charges .create({
       amount:500,
       currency: 'usd',
       description: '$5 for 5 credits',
